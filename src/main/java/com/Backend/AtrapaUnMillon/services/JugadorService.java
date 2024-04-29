4f9dacc0937374feb401476e83a14301f52dd53c
hugomoreno16 <hugomoreno1614@gmail.com>
2024-04-29 11:21:33 +0200

    updated version

--- src/main/java/com/Backend/AtrapaUnMillon/services/JugadorService.java
+++ src/main/java/com/Backend/AtrapaUnMillon/services/JugadorService.java
@@ -4,14 +4,19 @@ import com.Backend.AtrapaUnMillon.exceptions.AdminBadRequestException;
 import com.Backend.AtrapaUnMillon.models.Jugador;
 import com.Backend.AtrapaUnMillon.models.Partida;
 import com.Backend.AtrapaUnMillon.repositories.JugadorRepository;
+import com.Backend.AtrapaUnMillon.repositories.PartidaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class JugadorService {
     @Autowired
     private JugadorRepository jugadorRepository;
 
+    @Autowired
+    private PartidaService partidaService;
     public Jugador crearJugador(String nickname, Partida partida){
         for(Jugador jugador : partida.getJugadores()){
             if(nickname.equals(jugador.getNombre())){
@@ -21,5 +26,20 @@ public class JugadorService {
         Jugador nuevo_jugador = new Jugador(nickname, partida);
         jugadorRepository.save(nuevo_jugador);
         return nuevo_jugador;
+    }
+
+    public Jugador getJugador(String partidaId, String nickname) {
+        Partida partida = partidaService.getPartida(partidaId);
+        Optional<Jugador> existing_jugador = jugadorRepository.findByNombreAndPartida(nickname, partida);
+        if(existing_jugador.isPresent()){
+            Jugador jugador = existing_jugador.get();
+            return jugador;
+        }else{
+            throw new AdminBadRequestException("Jugador no encontrado");
+        }
+    }
+
+    public void update(Jugador jugador) {
+        jugadorRepository.save(jugador);
     }
 }
